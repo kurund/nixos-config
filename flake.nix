9700707b81279e57cb1d76d82397d6b286b3de48
Kurund Jalmi <kurund.jalmi@gmail.com>
2026-09-20 21:38:02 +0100

    initial nixos setup

---
+++ flake.nix
@@ -0,0 +1,9 @@
+{
+  description = "NixOS setup";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
